b7ec99cd84d32818e3e3131fc444b9ef60dba410
ademiltonnunes <ademilton.marcelo1@gmail.com>
2024-07-18 14:41:15 -0700

    Creating a backend folder instead

--- src/pages/Chat/index.js
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import {Button, Grid} from '@material-ui/core';
-import TextArea from '@mui/joy/TextArea';
-import Images from '../../constants/images';
-import ChatStyle from '../../styles/chat';
-import ReXMessagge from '../../components/ReXMessagge';
-import api from '../../api/sessions';
-import OpenAI from 'openai-api';
